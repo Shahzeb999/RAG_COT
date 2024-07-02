b8c559d858421ea0ea42cf55d622cefedf9d0229
Shahzeb999 <57065715+Shahzeb999@users.noreply.github.com>
2024-07-02 16:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def process_query():
         return jsonify({'status': 'error', 'message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
